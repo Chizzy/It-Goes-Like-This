5d09e08f925b911bd68f7c353d1da1d633a44812
Chizzy <chizoba.princess@gmail.com>
2018-11-15 11:11:00 -0500

    added spotify authorization and backend files/folders

---
+++ client/src/components/LogInPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import styled from 'styled-components'
+
+class LogIn extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from Log In Page</h1>
+            </div>
+        );
+    }
+}
+
+export default LogIn;
