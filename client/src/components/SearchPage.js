ad69dd247798022e53539ac98d877ef39b939b77
Chizzy <chizoba.princess@gmail.com>
2018-11-21 00:07:58 -0500

    centered search bar

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -1,37 +1,50 @@
 import React, { Component } from "react";
 import axios from "axios";
-import SearchResults from './SearchResults'
+import SearchResults from "./SearchResults";
 import Spotify from "spotify-web-api-js";
+import styled from "styled-components";
 
 const spotifyWebApi = new Spotify();
 
-class SearchPage extends Component {
-    constructor() {
-        super();
-        const params = localStorage.getItem("token");
-        this.state = {
-          loggedIn: params ? true : false,
-          user: {
-            id: "",
-            name: ''
-          },
-        search: '',
-        results: []
-        };
-        if (params) {
-          spotifyWebApi.setAccessToken(params);
-        }
-      }
+const SearchStyles = styled.div`
+  @import url("https://fonts.googleapis.com/css?family=Fredoka+One|Monoton");
+  font-family: "Fredoka One", cursive;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+    button {
+        margin-top: 1rem;
+    }
+`;
 
-      getUser = () => {
-        spotifyWebApi.getMe().then(response => {
-          this.setState({
-            user: {
-              id: response.id,
-            }
-          });
-        });
-      };
+class SearchPage extends Component {
+  constructor() {
+    super();
+    const params = localStorage.getItem("token");
+    this.state = {
+      loggedIn: params ? true : false,
+      user: {
+        id: "",
+        name: ""
+      },
+      search: "",
+      results: []
+    };
+    if (params) {
+      spotifyWebApi.setAccessToken(params);
+    }
+  }
+
+  getUser = () => {
+    spotifyWebApi.getMe().then(response => {
+      this.setState({
+        user: {
+          id: response.id
+        }
+      });
+    });
+  };
 
   handleSearchInput = event => {
     const name = event.target.name;
@@ -42,36 +55,46 @@ class SearchPage extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    const token = '';
+    const token =
+      "";
     const search = this.state.search.replace(/\s/, "%20");
     const url = `https://api.genius.com/search?q=${search}&access_token=${token}`;
-    axios.get(url).then(response => {
-      const results = response.data.response.hits
-      this.setState({results: results})
-    }).catch(err => {
-        console.log(err)
-    })
+    axios
+      .get(url)
+      .then(response => {
+        const results = response.data.response.hits;
+        this.setState({ results: results });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   render() {
     return (
-      <div>
+      <SearchStyles>
         <form onSubmit={this.handleSubmit}>
-          <div>
+          <div className="form-row align-items-center">
+          <div className="col-auto">
+          <label htmlFor="search">Song Stuck In Your Head? </label>
             <input
               type="text"
               name="search"
               placeholder="Enter lyrics here..."
               value={this.state.search}
               onChange={this.handleSearchInput}
+              className="form-control"
             />
-            <button type="submit">Search</button>
+          </div> 
           </div>
-          <div>
-              <SearchResults results={this.state.results} />
-          </div>
+          <button type="submit" className="btn button-white col-auto">
+              Search
+            </button>
         </form>
-      </div>
+        <div>
+          <SearchResults results={this.state.results} />
+        </div>
+      </SearchStyles>
     );
   }
 }
