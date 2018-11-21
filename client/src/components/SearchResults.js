1f23ae79afac99233dc039948142134ce1b68d0d
Chizzy <chizoba.princess@gmail.com>
2018-11-21 00:53:10 -0500

    bootstrapped flex boxed results

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
 import Spotify from "spotify-web-api-js";
 import axios from "axios";
+import styled from "styled-components";
 
 const spotifyWebApi = new Spotify();
+
+const CardStyle = styled.div`
+  color: black;
+  width: 275px;
+  margin-top: 2rem;
+    a {
+        text-decoration: none;
+        color: black;
+    }
+    h2:hover {
+        text-shadow: 5px 5px #85FFC7;
+    }
+`;
 
 class Results extends Component {
   constructor() {
@@ -32,52 +46,78 @@ class Results extends Component {
         const matchedSongs = response.data.tracks.items;
         this.setState({ matchedSongs: matchedSongs });
       });
-      this.setState({ showMatchedSongs: !this.state.showMatchedSongs });
+    this.setState({ showMatchedSongs: !this.state.showMatchedSongs });
   };
 
-  refreshPage =() =>{ 
-    window.location.reload(); 
-}
+  refreshPage = () => {
+    window.location.reload();
+  };
 
   render() {
     const results = this.props.results.map((result, i) => {
       return (
-        <div className='card' key={i}>
-          <img className="card-img-top"
+        <CardStyle className="card" key={i}>
+          <img
+            className="card-img-top"
             src={result.result.primary_artist.image_url}
             alt={result.result.title}
+            width="275"
+            height="275"
           />
-            <h2 onClick={() => this.getTrack(i)}>
+          <div className="card-body">
+            <h2 onClick={() => this.getTrack(i)} className="card-title">
               {result.result.title}
             </h2>
-          <h3>{result.result.primary_artist.name}</h3>
-        </div>
+            <h3 className="card-subtitle mb-2 text-muted">
+              {result.result.primary_artist.name}
+            </h3>
+          </div>
+        </CardStyle>
       );
     });
 
     const matchedSongs = this.state.matchedSongs.map((song, i) => {
       return (
-        <div key={i}>
-          <img src={song.album.images[0].url} alt={song.name} />
-          <a onClick={this.refreshPage} href={song.external_urls.spotify} target="_blank" rel="noopener noreferrer">
-            <h2>{song.name}</h2>
-          </a>
-          <h3>{song.artists[0].name}</h3>
-        </div>
+        <CardStyle className="card" key={i}>
+          <img
+            className="card-img-top"
+            src={song.album.images[0].url}
+            alt={song.name}
+          />
+          <div className="card-body">
+            <a
+              onClick={this.refreshPage}
+              href={song.external_urls.spotify}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className="card-title">{song.name}</h2>
+            </a>
+            <h3 className="card-subtitle mb-2 text-muted">
+              {song.artists[0].name}
+            </h3>
+          </div>
+        </CardStyle>
       );
     });
 
     return (
       <div>
-        {this.state.showMatchedSongs ?
-        (<div>
+        {this.state.showMatchedSongs ? (
+          <div>
             <h1>Matched Songs</h1>
+            <div className="d-flex flex-wrap justify-content-center justify-content-around">
             {matchedSongs}
-          </div>) :
-          (<div>
+            </div>
+          </div>
+        ) : (
+          <div>
             <h1>Results of Searched Lyrics</h1>
-            {results}
-          </div>)}
+            <div className="d-flex flex-wrap justify-content-center justify-content-around">
+              {results}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
